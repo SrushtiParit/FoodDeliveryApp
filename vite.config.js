25e5918c6cca8b714025a4236766d97c9423003a
Srushti Parit <102237808+SrushtiParit@users.noreply.github.com>
2025-06-29 19:23:34 +0000

    addedProjectToCodeSandbox

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["t3f8qg-5173.csb.app"],
+  },
+});
